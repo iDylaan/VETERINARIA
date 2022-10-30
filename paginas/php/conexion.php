815eac6c4abcbac0a063ce36a495b33dfe4fe07c
brandoncf7 <108553422+brandoncf7@users.noreply.github.com>
2022-10-30 10:36:14 -0600

    avance

---
+++ paginas/php/conexion.php
@@ -0,0 +1,10 @@
+<?php
+    $host - "localhost";
+    $User
+             "root";
+    $pass
+    $db - "Intclosesiando";
+    Sconexion - mysqli_connect($host, $User F $pass, $db);
+    if ($$con){
+    echo "Conexion fallida";
+}
